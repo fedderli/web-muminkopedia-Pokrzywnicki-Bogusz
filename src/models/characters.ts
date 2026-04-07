29a1c5d06cd1ef4937068d8a24da17e370929f3e
fedderli <pokrzywnickikarol@gmail.com>
2026-04-07 15:51:36 +0200

    stworzenie modelu dla postaci w characters.ts

---
+++ src/models/characters.ts
@@ -0,0 +1,21 @@
+﻿import mongoose, { Document } from "mongoose";
+
+export interface Character extends Document {
+    name: string;
+    description: string;
+    species: string;
+    status:string;
+    bestFriend: string;
+    ownerOf: string[];
+}
+
+const CharacterSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    species: { type: String, required: true },
+    status: { type: String, required: true },
+    bestFriend: { type: String, required: true },
+    ownerOf: [{type: String}]
+})
+
+export default mongoose.model<Character>("Character", CharacterSchema);
