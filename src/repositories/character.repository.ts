49440317a5566987fe3c8af9244f6e834f91ce9e
fedderli <pokrzywnickikarol@gmail.com>
2026-04-08 18:48:05 +0200

    stworzono endpoint dododawania postaci, wyswietlania postaci, aktualizowania postaci i usuwania postaci uzywajac wzorca archektonicznego CSR

---
+++ src/repositories/character.repository.ts
@@ -0,0 +1,20 @@
+﻿import characterModel, {Character} from "../models/characters"
+import CharacterRoutes from "../routes/characterRoutes";
+
+export async function getAllCharacters(): Promise<Character[]> {
+
+    return characterModel.find()
+}
+
+export async function createCharacter(name:string, description:string, species: string, status: string, bestFriend: string, ownerOf :string[]) :Promise<Character> {
+    const newCharacter = new characterModel({name, description, species, status, bestFriend, ownerOf});
+    return await newCharacter.save();
+}
+
+export async function updateCharacterById(id:string,updatedData: Partial<Character>):Promise<Character | null> {
+    return characterModel.findByIdAndUpdate(id, updatedData, {returnDocument: 'after'})
+}
+
+export async function deleteCharacterById(id:string):Promise<Character | null> {
+    return await characterModel.findByIdAndDelete(id)
+}
--- src/routes/characterRoutes.ts
+++ src/routes/characterRoutes.ts
@@ -1,36 +1,15 @@
 ﻿import express, {Request,Response} from "express";
 import CharacterModel, {Character} from "../models/characters";
+import {deleteCharacter, getCharacters, postCharacter, putCharacter} from "../controllers/character.controller";
 
 
 const router = express.Router();
 
-router.get('/',async (req:Request, res : Response) => {
-    try{
-        const characters: Array<Character> = await CharacterModel.find();
-        res.json(characters);
-    } catch(err){
-        res.status(500).json({error:`nie udalo sie pobrac danych ${err}`});
-    }
-})
+router.get('/',getCharacters);
+router.post('/',postCharacter);
+router.put('/:id',putCharacter);
+router.delete('/:id',deleteCharacter);
 
-
-router.post('/', async (req:Request, res : Response) => {
-    try {
-        const {name, description, species, status, bestFriend, ownerOf} = req.body;
-        const newCharacter = new CharacterModel({
-            name,
-            description,
-            species,
-            status,
-            bestFriend,
-            ownerOf
-        });
-        await newCharacter.save();
-        res.status(201).json({message: `dodano nowa postac ${newCharacter}`});
-    }catch(err){
-        res.status(500).json({massage: "nie udalo sie dodac postaci", err})
-    }
-})
 
 
 export default router;
